2cb2191af473dea10fc4c50c80fcefdad5bfb4db
ShackGame <69221978+ShackGame@users.noreply.github.com>
2020-09-22 12:46:53 +0200

    Level 1 Alpha complete

---
+++ ZamaZama/Assets/Scripts/Environment/NextLevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class NextLevel : MonoBehaviour
+{
+    public string nextSceneAddress;
+
+    public Animator transition;
+
+    public float transtionTime = 2f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        transition.SetTrigger("start");
+
+        yield return new WaitForSeconds(transtionTime);
+
+        Addressables.LoadSceneAsync(nextSceneAddress);
+    }
+}
