5a8ccf0926bd9aaeb19cbb08e50ccec201911a13
ShackGame <69221978+ShackGame@users.noreply.github.com>
2020-09-24 14:15:06 +0200

    Health and Menu
    Health pick pick up that increases player's health,  end of level proceeds to  the correct level

---
+++ ZamaZama/Assets/Scripts/Environment/HealthPickUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickUp : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GameManager.Health += 2f;
+            Destroy(gameObject);
+        }
+    }
+}
